292290c447680087ee3d90665bf2485e944a1006
lennartajansson <lennart.a.jansson@outlook.com>
2025-04-21 17:06:51 +0200

    Add project files.

---
+++ Listener/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Client.JetStream.Models;
+
+NatsConnection? connection;
+INatsJSContext? jetStreamContext;
+INatsJSStream? jetStream;
+INatsJSConsumer? consumer;
+
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+
+NatsOpts opts = NatsOpts.Default with
+{
+  Url = $"nats://nats.ubk3s:4222",
+  LoggerFactory = factory
+};
+
+connection = new NatsConnection(opts);
+jetStreamContext = new NatsJSContext(connection);
+jetStream = await jetStreamContext!.CreateStreamAsync(new StreamConfig("teststream", subjects: new string[] { "teststream.new" }));
+consumer = await jetStreamContext.CreateOrUpdateConsumerAsync("teststream", new ConsumerConfig("testconsumer2"));
+
+CancellationTokenSource cts = new();
+
+Console.CancelKeyPress += (_, e) =>
+{
+  e.Cancel = true;
+  cts.Cancel();
+};
+
+//Read all items from the channel.
+await foreach (NatsJSMsg<string> jsMsg in consumer!.ConsumeAsync<string>(cancellationToken: cts.Token))
+{
+  if (jsMsg.Data is not null)
+  {
+    Console.WriteLine($"Received message: {jsMsg.Data}");
+    await jsMsg.AckAsync();
+  }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Client.JetStream.Models;
+
+NatsConnection? connection;
+INatsJSContext? jetStreamContext;
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+
+NatsOpts opts = NatsOpts.Default with
+{
+  Url = "nats://nats.ubk3s:4222",
+  LoggerFactory = factory
+};
+
+connection = new NatsConnection(opts);
+jetStreamContext = new NatsJSContext(connection);
+
+_ = await jetStreamContext!.CreateStreamAsync(new StreamConfig("teststream", subjects: new string[] { "teststream.new" }));
+
+
+CancellationTokenSource cts = new();
+
+Console.CancelKeyPress += (_, e) =>
+{
+  e.Cancel = true;
+  cts.Cancel();
+};
+
+while (!cts.IsCancellationRequested)
+{
+  // Publish a message to the stream
+  string message = $"Hello world {DateTime.Now}";
+  PubAckResponse ack = await jetStreamContext!.PublishAsync(subject: "teststream.new", data: message);
+  ack.EnsureSuccess();
+  await Task.Delay(5000);
+}
+
